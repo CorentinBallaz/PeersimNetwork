f0b6edfbc19d34729a9760b7d263abeb7c50f7a7
CorentinBallaz <corentin.ballaz@yahoo.fr>
2020-03-25 21:28:11 +0100

    ajout des voisins

--- src/virus/Initializer.java
+++ src/virus/Initializer.java
@@ -1,4 +1,6 @@
 package virus;
+import java.util.ArrayList;
+import java.util.Random;
 
 import peersim.edsim.*;
 import peersim.core.*;
@@ -13,8 +15,90 @@ public class Initializer implements peersim.core.Control {
 
 
     public Initializer(String prefix) {
-	//recuperation du pid de la couche applicative
+    //recuperation du pid de la couche applicative
+        System.out.println(Network.size());
         this.virusAppPid = Configuration.getPid(prefix+ ".virusAppProtocolPid");
+        
+    }
+    public void ajoutVoisins(){
+        int minVoisins = 1;//Configuration.getPid("nbVoisinsMin");
+        int maxVoisins = 10;//Configuration.getPid("nbVoisinsMax");
+        int range = maxVoisins - minVoisins + 2;
+        int rangeNetwork = Network.size();
+        for(int i=0;i<rangeNetwork;i++){
+            Node myNode = Network.get(i);
+            // on recupere la couche applicative et on lui associe le numéro de son noeud
+            VirusApp destApp = (VirusApp)myNode.getProtocol(this.virusAppPid);
+            ArrayList listVoisins = new ArrayList<>();
+            for(int k=0;k<rangeNetwork;k++){
+                if(k!=i){
+                    Node currentNode = Network.get(k);
+                    VirusApp currentDestApp = (VirusApp)currentNode.getProtocol(this.virusAppPid);
+                    try{
+                        for(int l=0;l<currentDestApp.getListVoisins().size();l++){
+                            int numVoisin = (int)currentDestApp.getListVoisins().get(l);
+                            boolean isIn = false;
+                            for(int m=0;m<destApp.getListVoisins().size();m++){
+                                if(destApp.getListVoisins().get(m).equals(numVoisin)){
+                                    isIn = true;
+                                }
+                            }
+                            if(currentDestApp.getListVoisins().get(l).equals(i) && numVoisin!=i && isIn==false){
+                                destApp.addVoisins(numVoisin);
+                            }
+                        }
+                    }catch(Exception e){}
+                }
+            }
+            int voisinAct;
+            try{
+                voisinAct = destApp.getListVoisins().size();
+            }catch(Exception e){
+                voisinAct = 0;
+            }
+            int randVoisins = (int)(Math.random() * range) + minVoisins - voisinAct;
+            if(randVoisins<0){
+                randVoisins=0;
+            }
+            int[] ints = new Random().ints(0,rangeNetwork).distinct().limit(randVoisins).toArray();
+            //System.out.println(ints.length);
+            for(int j=0;j<ints.length;j++){
+                int monNb = ints[j];
+                boolean isIn2 = false;
+                try{
+                    for(int n=0;n<destApp.getListVoisins().size();n++){
+                        if(destApp.getListVoisins().get(n).equals(monNb)){
+                            isIn2 = true;
+                        }
+                    }
+                }catch(Exception e){}
+                if(ints[j]!=i && isIn2==false){
+                    //System.out.println("noeud : " + i + "; voisin : " + monNb);
+                    Node verifNode = Network.get(monNb);
+                    VirusApp verifDestApp = (VirusApp)verifNode.getProtocol(this.virusAppPid);
+                    try{
+                        verifDestApp.addVoisins(i);
+                        //System.out.println("j'ajoute");
+                    }catch(Exception e){
+                        ArrayList listVoisins2 = new ArrayList<>();
+                        listVoisins2.add(i);
+                        verifDestApp.setListVoisins(listVoisins2);
+                        //System.out.println("Je crée");
+                    }
+                    //System.out.println("noeud ajouté : " + verifDestApp.getListVoisins().toString());
+                    try{
+                        destApp.addVoisins(monNb);
+                    }catch(Exception e){
+                        ArrayList listVoisins3 = new ArrayList<>();
+                        listVoisins3.add(monNb);
+                        destApp.setListVoisins(listVoisins3);
+                    }
+                    //System.out.println("noeud actuel : " + destApp.getListVoisins().toString());
+                }
+            }
+            destApp.setProbConta(Math.random());
+
+        }
     }
 
     public boolean execute() {
@@ -24,7 +108,9 @@ public class Initializer implements peersim.core.Control {
         VirusMessage virusMessage;
 		System.out.println("ça marche");
 		//Taille reseau
-		nodeNb = Network.size();
+        nodeNb = Network.size();
+        //ajout des voisins pour chaque noeud
+        this.ajoutVoisins();
 		//Creation message
         virusMessage = new VirusMessage(0,"Infected");
         if (nodeNb < 1) {
@@ -36,12 +122,12 @@ public class Initializer implements peersim.core.Control {
         appEmitter.setNodeId(0);
 
         // pour tous les autres noeuds du graph
-        for (int i = 1; i < nodeNb; i++) {
-
+        for (int i = 0; i < nodeNb; i++) {
             dest = Network.get(i);
             // on recupere la couche applicative et on lui associe le numéro de son noeud
             destApp = (VirusApp)dest.getProtocol(this.virusAppPid);
             destApp.setNodeId(i);
+            System.out.println(destApp.toString() + " " + destApp.getListVoisins().toString() + " " + destApp.getProbConta());
             //on envoit via la couche applicative au destinataire
             appEmitter.send(virusMessage, dest);
         }
--- src/virus/VirusApp.java
+++ src/virus/VirusApp.java
@@ -1,25 +1,62 @@
 package virus;
 
+import java.util.ArrayList;
+import java.util.Random;
 import peersim.edsim.*;
 import peersim.core.*;
 import peersim.config.*;
 
 public class VirusApp implements EDProtocol {
 
-
-    //identifiant de la couche courante (la couche applicative)
+    // identifiant de la couche courante (la couche applicative)
     private int mypid;
 
-    //le numero de noeud
+    // le numero de noeud
     private int nodeId;
 
-    //prefixe de la couche (nom de la variable de protocole du fichier de config)
+    // prefixe de la couche (nom de la variable de protocole du fichier de config)
     private String prefix;
 
-    public VirusApp(String prefix){
-         this.prefix = prefix;
+    // ajout de la probabilité de contaminer quelqu'un
+    private double probConta;
+
+    // ajout de la liste de voisins pour de petits noeuds
+    private ArrayList listVoisins;
+
+    public VirusApp(String prefix) {
+        this.prefix = prefix;
         this.mypid = Configuration.getPid(prefix + ".myself");
     }
+
+    public double getProbConta() {
+        return probConta;
+    }
+
+    public void setProbConta(double probConta) {
+        this.probConta = probConta;
+    }
+
+    public ArrayList getListVoisins() {
+        return listVoisins;
+    }
+
+    public void setListVoisins(ArrayList listVoisins) {
+        this.listVoisins = listVoisins;
+    }
+    public void addVoisins(int numVoisins){
+        this.listVoisins.add(numVoisins);
+    }
+    // public void addListVoisins(){
+    //     int minVoisins = 1;//Configuration.getPid("nbVoisinsMin");
+    //     int maxVoisins = 5;//Configuration.getPid("nbVoisinsMax");
+    //     int range = maxVoisins - minVoisins + 1;
+    //     int randVoisins = (int)(Math.random() * range) + minVoisins;
+    //     int rangeNetwork = Network.size();
+    //     int[] ints = new Random().ints(1, rangeNetwork).distinct().limit(randVoisins).toArray();
+    //     for(int i=0;i<ints.length;i++){
+    //         System.out.println(ints[i]);
+    //     }         
+    // }
 
     public Object clone() {
         VirusApp dolly = new VirusApp(this.prefix);
@@ -31,6 +68,9 @@ public class VirusApp implements EDProtocol {
     }
     private Node getMyNode() {
         return Network.get(this.nodeId);
+    }
+    public int getMyID(){
+        return this.nodeId;
     }
     public String toString() {
         return "Node "+ this.nodeId;
